400f6958907aeb8dc6d93cbbf8c5560b37d8f064
JaswanthReddyPadala <jaswanthreddypadala@gmail.com>
2021-08-05 17:27:11 +0530

    add: one by one user input method

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ const userCash = document.getElementById("user_cash_input");
 const totalBill = document.getElementById("total_bill_input");
 const denominationGroup = document.getElementById("denomination-group");
 
+const totalBillGroup = document.querySelector(".total-bill");
+
+const nextButton = document.getElementById("calculate_button");
+
+const appOutput = document.querySelector(".app_output");
+
+nextButton.addEventListener("click", nextInput);
+
+function nextInput() {
+  if (userCash.value > 0) {
+    nextButton.innerText = "Calculate";
+    nextButton.addEventListener("click", checkInputs);
+    totalBillGroup.style.display = "flex";
+  }
+  // if(userCash.value > 0){
+  //   const totalBillH1 = document.createElement("h1");
+  //   const totalBillInput = document.createElement("input");
+  //   totalBillInput.type = "number";
+  //   totalBillInput.id="total_bill_input";
+  //   totalBillInput.autocomplete = "off";
+
+  //   totalBillGroup.appendChild(totalBillH1);
+  //   totalBillGroup.appendChild(totalBillInput);
+}
+
 function checkInputs() {
   userCashAmount = Number(userCash.value);
   totalBillAmount = Number(totalBill.value);
@@ -37,17 +62,16 @@ function calculateReturn(userCashAmount, totalBillAmount) {
   for (let i = denominationKeys.length; i >= 0; i--) {
     let denominationAmount = denominationKeys[i - 1];
     let changeAmount = change / denominationAmount;
-    if (changeAmount > 1) {
+    if (changeAmount >= 1) {
       denomination[denominationAmount] += Math.floor(changeAmount);
       change -= denominationAmount * Math.floor(changeAmount);
     }
-    console.log(change);
   }
-  console.log(denomination);
   renderReturn(denomination, denominationKeys);
 }
 
 function renderReturn(denomination, dkeys) {
+  appOutput.style.display = "flex";
   denominationGroup.innerHTML = "";
   dkeys.map((eachItem) => {
     if (denomination[eachItem] > 0) {
